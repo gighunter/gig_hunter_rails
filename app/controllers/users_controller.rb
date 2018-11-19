adef7a016e7a82626ab8fa77c8ad0830f67337c1
allinthemind66 <r.nilon92@gmail.com>
2018-11-18 22:12:28 -0500

    added show action to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
+  end
 end
