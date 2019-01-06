40351c188e2b4f5f93e0bb4a137dd9fada3e2600
allinthemind66 <r.nilon92@gmail.com>
2019-01-05 19:41:16 -0500

    add create action for user

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,7 +4,8 @@ class AuthenticationController < ApplicationController
  def authenticate
    command = AuthenticateUser.call(params[:email], params[:password])
    if command.success?
-     render json: { auth_token: command.result }
+     @user_id = User.find_by(email: params[:email]).id
+     render json: { auth_token: command.result, user_id: @user_id}
    else
      render json: { error: command.errors }, status: :unauthorized
    end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     render json: @user
   end
+
+  def create
+    @user = User.create(user_name)
+    @user.img_url = 'https://cdn.filestackcontent.com/ehIcNumRke6Mn1dp6kjA'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :city, :first_name, :last_name)
+  end
 end
