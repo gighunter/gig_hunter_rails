a807e64c745a89afdfa7e9d6dc8721df984bfad5
allinthemind66 <r.nilon92@gmail.com>
2019-01-07 22:27:47 -0500

    create new directory for api/v1 and move all files into directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'authenticate', to: 'authentication#authenticate'
-  resources :users
+  namespace :api do
+    namespace :v1 do
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      post 'authenticate', to: 'authentication#authenticate'
+      resources :users
+    end
+  end
 end
