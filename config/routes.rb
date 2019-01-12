a720a25f6a96f216bd59a5d2dd10b43dcd882387
allinthemind66 <r.nilon92@gmail.com>
2019-01-12 10:22:58 -0500

    clean up routes by excluding new and edit from user resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
       post 'authenticate', to: 'authentication#authenticate'
-      resources :users
+      resources :users, :except => [:new, :edit]
     end
   end
 end
